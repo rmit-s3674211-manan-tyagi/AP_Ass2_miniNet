f19d05d26d4be5bf4068f2b37de3b52ed442511d
Manan Tyag <31241399+rmit-s3674211-manan-tyagi@users.noreply.github.com>
2018-05-20 16:22:58 +1000

    Add files via upload

---
+++ src/model/Adult.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Adult extends User{
+	
+	 public Adult(String name, int age, String state, String gender) {
+		 super(name, age,state,gender);
+		}
+
+	    public Adult() {
+	        super();
+	    }
+}
