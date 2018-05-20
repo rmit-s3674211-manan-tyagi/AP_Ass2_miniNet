029f419cb2c1ffb8b9382c2c64005d7f75ccef66
Manan Tyag <31241399+rmit-s3674211-manan-tyagi@users.noreply.github.com>
2018-05-20 17:23:26 +1000

    Add files via upload

---
+++ src/model/Child.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Child extends User{
+	private String parent_id1;
+    private String parent_id2;
+
+    public Child(String name, int age, String state, String gender,String parent_id1, String parent_id2) {
+		 super(name, age,state,gender);
+		 this.parent_id1 = parent_id1;
+	        this.parent_id2 = parent_id2;
+		}
+   
+
+    public Child()
+    {
+
+    }
+    public String getParent_id1() {
+        return parent_id1;
+    }
+
+    public void setParent_id1(String parent_id1) {
+        this.parent_id1 = parent_id1;
+    }
+
+    public String getParent_id2() {
+        return parent_id2;
+    }
+
+    public void setParent_id2(String parent_id2) {
+        this.parent_id2 = parent_id2;
+    }
+}
